1ce26fa20a3b85fc963a8d166eaa4c22cf20ddaf
Emanuel Gioda <ema.gioda1@gmail.com>
2026-02-09 01:06:36 -0300

    Update

--- app/src/main/java/com/emagioda/myapp/presentation/screen/diagnostic/util/PartsImageMap.kt
+++ app/src/main/java/com/emagioda/myapp/presentation/screen/diagnostic/util/PartsImageMap.kt
@@ -3,11 +3,11 @@ package com.emagioda.myapp.presentation.screen.diagnostic.util
 import com.emagioda.myapp.R
 
 val PartsImageMap: Map<String, Int> = mapOf(
+    "silo_mv18" to R.drawable.silo_mv18,
     "img_p_01" to R.drawable.img_p_01,
     "img_p_02" to R.drawable.img_p_02,
     "img_p_04" to R.drawable.img_p_04,
     "img_p_06" to R.drawable.img_p_06,
     "img_p_07" to R.drawable.img_p_07,
-    "img_p_08" to R.drawable.img_p_08,
-    "silo_mv18" to R.drawable.silo_mv18
+    "img_p_08" to R.drawable.img_p_08
 )
